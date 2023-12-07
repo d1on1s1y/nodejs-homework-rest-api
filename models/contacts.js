d760d0af7d201619666d6de526b7e388746886f9
Денис Бурдин <dionisiy.god.of.wine@gmail.com>
2023-12-07 17:39:35 +0200

    .

--- models/contacts.js
+++ models/contacts.js
@@ -1,66 +1,40 @@
-const fs = require("fs/promises");
-const path = require("path");
-const { nanoid } = require("nanoid");
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
-const contactsPath = path.join(__dirname, "contacts.json");
+const { handleMongooseError } = require("../helpers");
 
-async function listContacts() {
-  const contacts = await fs.readFile(contactsPath);
-  return JSON.parse(contacts);
-}
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-async function getContactById(contactId) {
-  const contacts = await listContacts();
-  const res = contacts.find((contact) => contact.id === contactId);
-  return res || null;
-}
+contactSchema.post("save", handleMongooseError);
 
-async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const id = contacts.findIndex((contact) => contact.id === contactId);
-  if (id === -1) {
-    return null;
-  }
-  const [result] = contacts.splice(id, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return result;
-}
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string(),
+  phone: Joi.string(),
+  favorite: Joi.boolean(),
+});
 
-async function addContact(data) {
-  const contacts = await listContacts();
-  const newContact = {
-    id: nanoid(),
-    ...data,
-  };
-  contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return newContact;
-}
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
 
-const updateContact = async (contactId, body) => {
-  if (!body) {
-    return null;
-  }
-  const contacts = await listContacts();
-  const contact = await getContactById(contactId);
-  if (!contact) {
-    return null;
-  }
-  const newContact = Object.assign(contact, body);
-  contacts.splice(
-    contacts.findIndex((contact) => contact.id === contactId),
-    1,
-    newContact
-  );
+const schemas = { addSchema, updateFavoriteSchema };
 
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return newContact;
-};
+const Contact = model("contact", contactSchema);
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-};
+module.exports = { Contact, schemas };
--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+const PASSWORD = "UZjKxPZUM2hW01pj";
+const DB_HOST = `mongodb+srv://Denis:${PASSWORD}@cluster0.cqmizph.mongodb.net/`;
+const PORT = 3000;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(PORT, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
